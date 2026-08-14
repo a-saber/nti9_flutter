699a36864643189a9cf4152945ea76c20bcee6a7
a-saber <ahs629862@gmail.com>
2026-08-14 11:44:16 +0300

    ui 02

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,72 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomeScreen());
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.grey,
-        leading: Icon(Icons.arrow_back),
+        leading: Icon(Icons.arrow_back, color: Colors.green),
+        title: Text("Home Screen", style: TextStyle(fontSize: 25)),
         centerTitle: true,
-        title: Text('Home'),
-        actions: [
-          Text('test'),
-          SizedBox(width: 20,),
-          Icon(Icons.check_circle)
-        ],
+        actions: [Text('Hi'), SizedBox(width: 20), Icon(Icons.check_circle)],
       ),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        crossAxisAlignment: CrossAxisAlignment.end,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
-              "Hello World",
-            style: TextStyle(
-              color: Colors.blue,
-              fontSize: 45,
-              fontWeight: FontWeight.w900,
-              letterSpacing: -5,
-              wordSpacing: -10,
-              decoration: TextDecoration.underline,
-              decorationStyle: TextDecorationStyle.wavy,
-              decorationColor: Colors.red,
-              backgroundColor: Colors.yellow,
+          Container(
+              color: Colors.green,
+              child: Text('data data data data data data ')),
+          SizedBox(height: 20),
+          Container(
+            padding: const EdgeInsets.only(
+                top: 10,
+                left: 10
             ),
-            textAlign: TextAlign.center,
-            maxLines: 2,
-            overflow: TextOverflow.fade,
+            margin: const EdgeInsets.symmetric(
+              horizontal: 20
           ),
-          SizedBox(height: 20,),
-          Icon(Icons.add_box_rounded, color: Colors.green,size: 25,)
-          
+            width: 100,
+            height: 100,
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              borderRadius: BorderRadius.circular(10),
+              // shape: BoxShape.circle,
+              // gradient: LinearGradient(
+              //   begin: Alignment.topCenter,
+              //   end: Alignment.bottomCenter,
+              //   colors: [Colors.black, Colors.grey, Colors.black],
+              // ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withValues(alpha: 0.5),
+                  spreadRadius: 5,
+                  blurRadius: 10,
+                  offset: Offset(0, -5)
+                )
+              ]
+
+            ),
+            alignment: Alignment.topLeft,
+            child: Icon(Icons.two_k_plus_sharp),
+          ),
         ],
       ),
-    ),
-    debugShowCheckedModeBanner: false,
-  ));
+    );
+  }
 }
