bbc11983ea0e0599ee6323e5f37a3142620620ea
a-saber <ahs629862@gmail.com>
2026-09-05 10:54:56 +0300

    fonts

--- lib/main.dart
+++ lib/main.dart
@@ -29,41 +29,13 @@ class HomeScreen extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Container(
-              color: Colors.green,
-              child: Text('data data data data data data ')),
-          SizedBox(height: 20),
-          Container(
-            padding: const EdgeInsets.only(
-                top: 10,
-                left: 10
+          Text(
+            'Hello World',
+            style: TextStyle(
+              fontFamily: 'Lexend_Deca',
+              fontWeight: FontWeight.w100,
+              fontSize: 50,
             ),
-            margin: const EdgeInsets.symmetric(
-              horizontal: 20
-          ),
-            width: 100,
-            height: 100,
-            decoration: BoxDecoration(
-              color: Colors.blue,
-              borderRadius: BorderRadius.circular(10),
-              // shape: BoxShape.circle,
-              // gradient: LinearGradient(
-              //   begin: Alignment.topCenter,
-              //   end: Alignment.bottomCenter,
-              //   colors: [Colors.black, Colors.grey, Colors.black],
-              // ),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withValues(alpha: 0.5),
-                  spreadRadius: 5,
-                  blurRadius: 10,
-                  offset: Offset(0, -5)
-                )
-              ]
-
-            ),
-            alignment: Alignment.topLeft,
-            child: Icon(Icons.two_k_plus_sharp),
           ),
         ],
       ),
