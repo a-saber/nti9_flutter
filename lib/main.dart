707024c531a3626b5762fbde7b6bd82b64c57ca2
a-saber <ahs629862@gmail.com>
2026-09-06 13:57:20 +0300

    scroll

--- lib/main.dart
+++ lib/main.dart
@@ -26,152 +26,23 @@ class MyApp extends StatelessWidget {
 }
 
 class HomeScreen extends StatelessWidget {
-  HomeScreen({super.key});
+  const HomeScreen({super.key});
 
-  var controller = TextEditingController();
-  List<int> numbers = [1, 2, 3, 4, 5];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-
-          Image.asset(AppImages.flag,
-            width: double.infinity,
-            height: 298.h,
-            fit: BoxFit.cover,
-          ),
-          SizedBox(height: 30,),
-
-          
-          Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: TextFormField(
-                controller: controller,
-                // enabled: false,
-                // readOnly: true,
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.grey,
-                  hintText: 'Enter Your Name',
-                  labelText: 'Name',
-                  enabledBorder: OutlineInputBorder(),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    borderSide: BorderSide(
-                      color: Colors.blue,
-                      width: 2
-                    )
-
-                  ),
-                  disabledBorder: UnderlineInputBorder(),
-                  // errorBorder: ,
-                  // focusedErrorBorder: ,
-                  // prefixIcon: Icon(Icons.title),
-                  prefixIcon: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: SvgPicture.asset(AppSvgs.profile,),
-                  ),
-                  // contentPadding: EdgeInsets.all(30),
-                  suffixIcon: Icon(Icons.local_airport)
-                ),
-                onChanged: (String value){
-                  print(value);
-                },
-                // onTap: (){
-                //   showTimePicker(context: context, initialTime: TimeOfDay.now());
-                // },
-                obscureText: true,
-                obscuringCharacter: '*',
-
-              )),
-          SizedBox(height: 30,),
-          Padding(
-            padding: REdgeInsets.symmetric(horizontal: 22.0),
-            child: SizedBox(
-              width: double.infinity,
-              // height: 150,
-              child: ElevatedButton(onPressed: (){
-                print(controller.text);
-              },
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(5),
-                    side: BorderSide(
-                      color: Colors.blue,
-                      width: 2
-                    )
-                  ),
-                  backgroundColor: Colors.grey,
-                  foregroundColor: Colors.black,
-                  elevation: 10,
-                  shadowColor: Colors.blue,
-                ), child: Text('Button 1'),
-
-              ),
-            ),
-          ),
-          TextButton(onPressed: (){
-            print('Text Button 01 pressed');
-          },
-              style: TextButton.styleFrom(
-                backgroundColor: Colors.grey,
-                foregroundColor: Colors.black,
-                elevation: 10,
-                shadowColor: Colors.blue,
-              ),
-              child: Text('Text Button 01')),
-          
-          IconButton(onPressed: (){}, icon: Icon(Icons.add_box_rounded)),
-          
-          
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: DropdownButtonFormField(
-                decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.grey,
-                    labelText: 'number',
-                    enabledBorder: OutlineInputBorder(),
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20),
-                        borderSide: BorderSide(
-                            color: Colors.blue,
-                            width: 2
-                        )
-
-                    ),
-                    disabledBorder: UnderlineInputBorder(),
-                    // errorBorder: ,
-                    // focusedErrorBorder: ,
-                    // prefixIcon: Icon(Icons.title),
-                    prefixIcon: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: SvgPicture.asset(AppSvgs.profile,),
-                    ),
-                ),
-                items: numbers.map(
-                    (int oldElement)=> DropdownMenuItem(value: oldElement, child: Row(
-                      children: [
-                        Icon(Icons.numbers),
-                        SizedBox(width: 10,),
-                        Text('$oldElement'),
-
-                      ],
-                    ),)
-                ).toList(),
-
-                onChanged: (value){
-                  print('selected value is $value');
-            }),
-          )
-
-          
-
-
-        ],
+      appBar: AppBar(
+        title: Text("Nti9 Flutter"),
+        centerTitle: true,
+      ),
+      body: ListView.separated(
+        itemBuilder: (context, index) =>  Container(
+          height: 200.h,
+          width: 200.w,
+          color: Colors.blue,
+        ),
+        separatorBuilder: (context, index) => SizedBox(height: 10.h,),
+        itemCount: 10
       ),
     );
   }
